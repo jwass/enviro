9bed4f5aeb57141408854d4ccd33e5b69ea8d0d8
Jacob Wasserman <jwasserman@gmail.com>
2015-02-23 14:52:59 -0500

    Run 'import os' in main environment in ipy ext
    When running the magic command on the command line, also make sure
    the os module is loaded to avoid typing 'import os' every time which
    you have to do anyway to make this of any use.

--- enviro_ipy_ext.py
+++ enviro_ipy_ext.py
@@ -7,6 +7,10 @@ def func(line):
     else:
         enviro.conf(line)
 
+    # Run 'import os' in the main namespace so that you don't have
+    # to do it explicitly
+    get_ipython().run_code('import os')
+
 
 def load_ipython_extension(ip):
     ip.register_magic_function(func, 'line', 'enviro')
