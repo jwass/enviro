38dacb1109abc6607f07773373dd5cb21d65c1d4
Jacob Wasserman <jwasserman@gmail.com>
2016-02-09 21:48:19 -0500

    Bump version
    This actually happened a long time ago and never got committed.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='enviro',
-      version='0.0.1',
+      version='0.0.2',
       description='Simple configuration of OS environment variables',
       long_description=long_description,
       classifiers=[],
